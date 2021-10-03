9b041bbf3125b109c590018bf31dcd6e398a7c96
Nap <70518391+Naffsisky@users.noreply.github.com>
2021-10-03 08:57:21 +0700

    Update Tugas Algo.rb

--- Tugas Algo.rb
+++ Tugas Algo.rb
@@ -4,11 +4,11 @@ int nilai
 #Deskripsi
 write("Masukan Nilai")
 read(nilai)
-if nilai =>80
+if nilai >=80
     write("Nilai A")
-elif nilai =>70 and <80
+elif nilai >=70 and <80
     write("Nilai B")
-elif nilai =>50 and <70
+elif nilai >=50 and <70
     write("Nilai C")
 elif nilai <50
     write("Nilai D")
