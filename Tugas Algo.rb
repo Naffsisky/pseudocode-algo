731e7fdcc2fe57c47ee0333a689bb41803ed65a5
Nap <70518391+Naffsisky@users.noreply.github.com>
2021-10-03 08:53:32 +0700

    Add files via upload

---
+++ Tugas Algo.rb
@@ -0,0 +1,16 @@
+#Deklarasi
+int nilai
+
+#Deskripsi
+write("Masukan Nilai")
+read(nilai)
+if nilai =>80
+    write("Nilai A")
+elif nilai =>70 and <80
+    write("Nilai B")
+elif nilai =>50 and <70
+    write("Nilai C")
+elif nilai <50
+    write("Nilai D")
+else
+    write("Masukkan nilai 1-100")
