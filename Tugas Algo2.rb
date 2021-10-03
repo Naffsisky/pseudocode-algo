868864f6ef67c1e5bcdc360db0c2b5b193819fea
Nap <70518391+Naffsisky@users.noreply.github.com>
2021-10-03 08:58:00 +0700

    Update Tugas Algo2.rb

--- Tugas Algo2.rb
+++ Tugas Algo2.rb
@@ -4,13 +4,13 @@ int nilaiipk
 #Deskripsi
 write("Masukan Nilai IPK")
 read(nilaiipk)
-if nilaiipk =>3.5
+if nilaiipk >=3.5
     write("Sangat Memuaskan")
-elif nilaiipk =>3.0 and <3.5
+elif nilaiipk >=3.0 and <3.5
     write("Memuaskan")
-elif nilaiipk =>2.5 and <3.0
+elif nilaiipk >=2.5 and <3.0
     write("Baik sekali")
-elif nilaiipk =>2.0 and <2.5
+elif nilaiipk >=2.0 and <2.5
     write("Baik")
 elif nilaiipk <2.0
     write("Kurang")
