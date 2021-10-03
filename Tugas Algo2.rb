604919c43b2f3c1ec48d14e28e58de1ea38873dc
Nap <70518391+Naffsisky@users.noreply.github.com>
2021-10-03 08:55:36 +0700

    Add files via upload

---
+++ Tugas Algo2.rb
@@ -0,0 +1,18 @@
+#Deklarasi
+int nilaiipk
+
+#Deskripsi
+write("Masukan Nilai IPK")
+read(nilaiipk)
+if nilaiipk =>3.5
+    write("Sangat Memuaskan")
+elif nilaiipk =>3.0 and <3.5
+    write("Memuaskan")
+elif nilaiipk =>2.5 and <3.0
+    write("Baik sekali")
+elif nilaiipk =>2.0 and <2.5
+    write("Baik")
+elif nilaiipk <2.0
+    write("Kurang")
+else
+    write("Masukkan nilai 0-4")
